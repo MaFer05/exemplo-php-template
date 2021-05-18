41b632129d9ca3c26fd1e5da3066ca424b87376f
MaFer05 <mariafernanda.05.oliveira@gmail.com>
2021-05-18 21:52:28 +0000

    Composer

--- includes/header.php
+++ includes/header.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8" />
         <title><?= $title ?></title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
-        <?php if(isset($styles)): ?>
+        <?php if(!is_null($styles)): ?>
             <link href="<?= 'css/' . $styles . '.css' ?>" rel="stylesheet" />
         <?php endif ?>
     </head>
