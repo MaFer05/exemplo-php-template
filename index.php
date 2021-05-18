68ad4d250e1ab079d437c87f047764e543f21d1d
MaFer05 <mariafernanda.05.oliveira@gmail.com>
2021-05-18 22:17:45 +0000

    Composer

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <?php 
-    include 'functions/template.php';
-    $title = 'Home Page';
+include 'vendor/autoload.php';
+
+$title = 'Home Page';
+$val = 10;
 ?>
 
 <?php getHeader('home') ?>
 
         <h1>Home Page</h1>
+        <?= soma($val, 20) ?>
         
 <?php getFooter() ?>
